a1647f278fe0294398c0c96891ff4b1481f0d28e
dbabichenko <dmb72@pitt.edu>
2017-01-04 14:05:32 -0500

    Initial commit
    Created new Unity Projects

---
+++ Class1Demo/Assets/Scripts/TowerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerController : MonoBehaviour {
+	public int speed;
+	public Rigidbody rg;
+
+	// Use this for initialization
+	void Start () {
+		speed = speed + 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
